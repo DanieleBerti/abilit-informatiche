7c1046fde43383ab0dc2229b141fe7bc582a9f24
walter bishop <95907486+DanieleBerti@users.noreply.github.com>
2021-12-10 10:44:32 +0000

    prova

---
+++ src/lib/numerictypes.py
@@ -0,0 +1,24 @@
+import sys
+import math
+
+print(sys.maxsize)
+
+print(math.pi)
+a=math.pi/6
+print(math.sin(a))
+
+a=1
+if(a):
+  print('True')
+print(type(a))
+a=True
+print(type(a))
+
+var1='Hello World!'
+print("var1[2]",var1[2])
+print("var1[0:7]",var1[0:7])
+print("var1[5]",var1[5])
+
+a= 'materia'
+print(a+a+a)
+
