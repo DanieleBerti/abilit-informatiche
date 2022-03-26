4b271fc20f74c1f07d4499b1c711fdd268ee97fa
walter bishop <95907486+DanieleBerti@users.noreply.github.com>
2022-03-26 15:52:17 +0000

    last but not least misses

--- src/lib/es5.py
+++ src/lib/es5.py
@@ -1,12 +1,43 @@
-a=input('primo numero:')
-b=input('secondo numero:')
-cont=True
-cont1=0
-while cont1<b:
-  if cont is True:
-   print('*'* a)
-   cont=False
+def funzione(a,b):
+ a=int(a)
+ b=int(b)
+ for i in range(b):
+   if i==0 or i==b-1:
+    for j in range(a):
+        print('*', end=' ')
+    print()
+   else:
+    for k in range(a):
+      if k==0 or k==a-1:
+        print('*', end=' ')
+      else:
+        print(' ', end=' ')
+    print()
+
+print("Ricorda di inserire numeri interi positivi e maggiori di 2, buon divertimento!")
+while True: 
+ while True:
+  par=True
+  a=input("lunghezza rettangolo:")
+  try:
+   a=int(a)
+  except:
+   par=False
+  if par is False or a<3:
+   print("Dato non valido, riprova")
+   break
   else:
-   print('*',''*(a-2),'*')
- cont1=cont1+1
-print('*'* a)
+   a=int(a)
+  par=True
+  b=input("altezza rettangolo:")
+  try:
+   b=int(b)
+  except:
+   par=False
+  if par is False or b<3:
+   print("Dato non valido, riprova")
+   break
+  else:
+   b=int(b)  
+  funzione(a,b)
+  break
