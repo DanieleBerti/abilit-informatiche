833f977753fef1b0789127c4e14bbb766f52150f
walter bishop <95907486+DanieleBerti@users.noreply.github.com>
2022-03-26 16:46:13 +0000

    fine senza revisione

--- src/lib/es6.py
+++ src/lib/es6.py
@@ -1,20 +1,26 @@
 from random import randrange
 a=True
+s=True
+cont=0
 print("Indovina il numero naturale che sto pensando, esso è compreso tra 1 e 10, estremi inclusi!")
 while a is True:
-  n=randrange(1, 10)
-  while True:
+ n=randrange(1, 10)
+ while a is True:
+  while s is True:
    c=input("numero tentativi:")
    par=True
    try:
-    b=int(b)
+    c=int(c)
    except:
     par=False
-   if par is False or b<1 or b>10:
+   if par is False or c<0:
       print("Non hai inserito un dato valido come numero di tentativi")
-      continue
    else:
+     c=int(c)
+     s=False
+     cont=0
      break
+  cont=cont+1
   while True:
    b=input("Guess:")
    par=True
@@ -24,14 +30,15 @@ while a is True:
     par=False
    if par is False or b<1 or b>10:
       print("Non hai inserito un dato valido come numero da indovinare")
-      continue
    else:
+     b=int(b)
      break
   if b==n:
     d=input("Hai indovinato,complimenti. Vuoi rigiocare ?[si/no]")
     if d=='si' or d=='no': 
       if d=='si':
-        continue
+        s=True
+        break
       else:
         a=False
         break
@@ -41,11 +48,41 @@ while a is True:
         d=input("Vuoi rigiocare ?[si/no]")
         if d=='si' or d=='no': 
           if d=='si':
-            continue
+            s=True
+            break
           else:
             a=False
             break
         else:
          print("risposta non valida")
          continue      
-  break
+    break 
+  else:
+    if cont==c:
+      d=input("mi dispiace, hai finito i tentativi disponibili. Vuoi rigiocare ? [si/no]")
+      if d=='si' or d=='no': 
+       if d=='si':
+         s=True
+         break
+       else:
+        a=False
+        break
+      else:
+       print("risposta non valida") 
+       while True:
+        d=input("Vuoi rigiocare ?[si/no]")
+        if d=='si' or d=='no': 
+          if d=='si':
+            s=True
+            break
+          else:
+            a=False
+            break
+        else:
+         print("risposta non valida")
+         continue      
+      break 
+    else:
+      print("risposta sbagliata, numero tentativi rimasti",c-cont)
+
+print("fine programma")
