da5d28d6fffdef6f4353e5ccf1f99cf2bc9027ec
walter bishop <95907486+DanieleBerti@users.noreply.github.com>
2022-03-26 15:04:51 +0000

    pr

---
+++ src/lib/es5.py
@@ -0,0 +1,12 @@
+a=input('primo numero:')
+b=input('secondo numero:')
+cont=True
+cont1=0
+while cont1<b:
+  if cont is True:
+   print('*'* a)
+   cont=False
+  else:
+   print('*',''*(a-2),'*')
+ cont1=cont1+1
+print('*'* a)
