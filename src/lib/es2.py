df30be69f87b1d72a55c77e3d850d8ec40f5bf00
walter bishop <95907486+DanieleBerti@users.noreply.github.com>
2022-03-29 16:11:32 +0000

    2,3,4,5,6

--- src/lib/es2.py
+++ src/lib/es2.py
@@ -1,10 +1,10 @@
 def funzione(el,lista=[]):
- if lib in lista:
-  print("Elemento trovato nella lista fornita con indice",lista.index(lib))
+ if el in lista:
+  print("Elemento trovato nella lista fornita con indice",lista.index(el))
  else:
   print("Elemento non trovato nella lista fornita")
 
-
+#esempi
 Mesi = ["Gennaio", "Febbraio", "Marzo","Aprile","Maggio","Giugno","Luglio","Agosto","Settembre","Ottobre","Novembre","Dicembre"]
 funzione(el = "Ottobre", lista = Mesi)
 funzione(el = "Mela", lista = Mesi)
